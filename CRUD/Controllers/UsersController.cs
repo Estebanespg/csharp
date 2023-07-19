1a50d2cde386edb627721e95704fdcf2431f8197
saffiro <srestrepo143@misena.edu.co>
2023-07-19 13:05:09 -0500

    Second Commit

---
+++ CRUD/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using CRUD.Data;
+using Microsoft.AspNetCore.Mvc;
+namespace CRUD.Controllers;
+
+[Route("users")]
+public class UsersController : Controller
+{
+
+    [HttpPost]
+    public ActionResult Create([FromBody] Models.UserModel user)
+    {
+
+        if (user.Id == 0)
+            return StatusCode(404, "ID cannot be 0");
+
+        if (user.Email.Length == 0)
+            return StatusCode(400, "Invalid Email");
+
+        if (user.Name.Length == 0)
+            return StatusCode(400, "Invalid Name");
+
+        if (user.PhoneNumber.Length == 0)
+            return StatusCode(400, "Invalid PhoneNumber");
+
+        var userData = Data.User.Create(user);
+
+        if (userData == false)
+            return StatusCode(400, "Failed");
+
+        return Ok();
+    }
+
+    [HttpGet]
+    public ActionResult Read([FromHeader] int id)
+    {
+        if (id == 0)
+            return StatusCode(404, "ID cannot be 0");
+
+        var userData = Data.User.Read(id);
+
+        if (userData.Id == 0)
+            return StatusCode(400, "Failed");
+
+        return Ok(userData);
+    }
+}
---
+++ CRUD/Data/User.cs
@@ -0,0 +1,60 @@
+﻿namespace CRUD.Data;
+public class User
+{
+    public static bool Create(Models.UserModel user)
+    {
+        try
+        {
+            var connection = new MySql.Data.MySqlClient.MySqlConnection("host=localhost; user=root; database=users; password=; port=3306;");
+            connection.Open();
+
+            var query = $""" INSERT INTO users(email, name, phoneNumber) VALUES('{user.Email}', '{user.Name}', '{user.PhoneNumber}') """;
+
+            var command = new MySql.Data.MySqlClient.MySqlCommand(query, connection);
+
+            command.ExecuteScalar();
+
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+
+
+    public static Models.UserModel Read(int id)
+    {
+        try
+        {
+            var connection = new MySql.Data.MySqlClient.MySqlConnection("host=localhost; user=root; database=users; password=; port=3306;");
+            connection.Open();
+
+            var query = $""" SELECT * FROM users WHERE id = {id} """;
+
+            var command = new MySql.Data.MySqlClient.MySqlCommand(query, connection);
+
+            var reader = command.ExecuteReader();
+            if (!reader.HasRows)
+                return new();
+            
+            while (reader.Read())
+            {
+                var user = new Models.UserModel
+                {
+                    Id = reader.GetInt32(0),
+                    Email = reader.GetString(1),
+                    Name = reader.GetString(2),
+                    PhoneNumber = reader.GetString(3)
+                };
+                return user;
+            }
+        }
+        catch
+        {
+            return new();
+        }
+        return new();
+    }
+}
